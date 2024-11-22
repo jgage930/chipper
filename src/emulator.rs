fe36cd7a55c8fd3fc7dda8270bf4c8e202ebfe0b
Jack Gage <jgage930@gmail.com>
2024-11-21 20:29:55 -0600

    first commit

---
+++ src/emulator.rs
@@ -0,0 +1,40 @@
+const RAM_SIZE: usize = 4096;
+const NUM_REGS: usize = 4096;
+const STACK_SIZE: usize = 16;
+
+const SCREEN_WIDTH: usize = 64;
+const SCREEN_HEIGHT: usize = 32;
+const START_ADDR: u16 = 0x200;
+const NUM_KEYS: usize = 16;
+
+pub struct Emulator {
+    // Current location in memory
+    pc: u16,
+    ram: [u8; RAM_SIZE],
+    screen: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
+    v_reg: [u8; NUM_REGS],
+    i_reg: u16,
+    sp: u16,
+    stack: [u16; STACK_SIZE],
+    keys: [bool; NUM_KEYS],
+    // Timers
+    dt: u8,
+    st: u8,
+}
+
+impl Emulator {
+    pub fn new() -> Self {
+        Self {
+            pc: START_ADDR,
+            ram: [0; RAM_SIZE],
+            screen: [false; SCREEN_HEIGHT * SCREEN_WIDTH],
+            v_reg: [0; NUM_REGS],
+            i_reg: 0,
+            sp: 0,
+            stack: [0; STACK_SIZE],
+            keys: [bool; NUM_KEYS],
+            dt: 0,
+            st: 0,
+        }
+    }
+}
